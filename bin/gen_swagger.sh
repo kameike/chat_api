7f8bc7509f1054d67e9e3f858adcb04be000ebb1
kameike <kameike@users.noreply.github.com>
2019-04-06 15:41:11 +0900

    add client gen code

--- bin/gen_swagger.sh
+++ bin/gen_swagger.sh
@@ -3,4 +3,5 @@
 set -eu
 
 swagger generate server -t swggen --exclude-main -f swagger.yml -A chat_api   --model-package="apimodel"
+swagger generate client -t swggen -f swagger.yml -A chat_api   --model-package="apimodel"
 
