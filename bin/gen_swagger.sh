abaa20e868e7205e5402b238e1febced948bcfb9
kameike <kameike@users.noreply.github.com>
2019-05-18 13:19:33 +0900

    update yaml

--- bin/gen_swagger.sh
+++ bin/gen_swagger.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-swagger generate server -t swggen --exclude-main -f swagger.yml -A chat_api   --model-package="apimodel"
-swagger generate client -t swggen -f swagger.yml -A chat_api   --model-package="apimodel"
+swagger generate server -t swggen --exclude-main -f swagger.yaml -A chat_api   --model-package="apimodel"
+swagger generate client -t swggen -f swagger.yaml -A chat_api   --model-package="apimodel"
 
